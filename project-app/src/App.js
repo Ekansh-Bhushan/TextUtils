ffa554192e39e0f8d919a04e325fd03eb41d214c
Ekansh <ekanshbhushan2k22@gmail.com>
2023-08-04 22:05:25 +0530

    Inital commit

---
+++ project-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+let name ="Ekansh"
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>about</li>
+      <li>contact</li>
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore itaque autem ut, sint voluptates voluptate repellendus, nam pariatur architecto assumenda facere perferendis blanditiis, provident iusto minus. Debitis quos quod, dicta cumque quam animi recusandae. Quisquam, dolore? Velit magnam distinctio exercitationem ipsam expedita impedit, a aperiam? Placeat perferendis, id voluptate sit, quos illo vitae praesentium inventore, repudiandae ratione adipisci voluptatum eveniet fuga. Ducimus, quibusdam enim?</p>
+    </nav>
+    </>
+  );
+}
+
+export default App;
