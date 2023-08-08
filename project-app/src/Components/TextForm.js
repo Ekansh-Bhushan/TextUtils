917ec0a5d3438df192d1bf242a517ae0e8765d18
Ekansh <ekanshbhushan2k22@gmail.com>
2023-08-08 13:12:39 +0530

    lowercase and more concept in textutiles

--- project-app/src/Components/TextForm.js
+++ project-app/src/Components/TextForm.js
@@ -11,6 +11,11 @@ export default function TextForm(props) {
     console.log("On changed.")
     setText(event.target.value)
   }
+  
+  const handleOncase =()=>{
+    let newLText = text.toLowerCase()
+    setText(newLText)
+  }
 
   const [text,setText] = useState('Enter Text Here')
   return (
@@ -21,13 +26,22 @@ export default function TextForm(props) {
   
 
         <textarea
-          className="form-control"
+          className="form-control my-3"
           value={text}
           onChange={handleOnChange}
           id="mybox"
           rows="8"
         ></textarea>
-        <button className="btn btn-primary" onClick={handleUpCase}>convert to uppercase</button>
+        <button className="btn btn-primary mx-3" onClick={handleUpCase}>convert to uppercase</button>
+        <button className="btn btn-primary" onClick={handleOncase}>Convert to lowercase</button>
+        </div>
+        <div className="container my-3">
+
+          <h1>Your text summary</h1>
+          <p>{text.split(" ").length} word and {text.length} charchter</p>
+          <p>{0.008* text.split(" ").length} Minutes to read</p>
+          <h3>preview</h3>
+          <p>{text}</p>
         </div>
       </div>
     </>
