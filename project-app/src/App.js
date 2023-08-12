983327385ead3471a6aeb274506bf14dc5f4a222
Ekansh-Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2023-08-12 13:02:07 +0530

    Router added

--- project-app/src/App.js
+++ project-app/src/App.js
@@ -1,42 +1,53 @@
 import { useState } from 'react';
 import './App.css';
-// import About from './Components/About';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import Alert from './Components/Alert';
 
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
-  const [mode, setMode] =useState('light') //wheter dark mode is on or not
+  const [mode, setMode] = useState('light') //wheter dark mode is on or not
   const [alert, setAlert] = useState(null)
 
-  const showAlert =(message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
     })
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null);
-    },1500)
+    }, 1500)
   }
-  const toggleMode=()=>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark')
-      document.body.style.backgroundColor ='black'
-      showAlert(" light mode","success :")
+      document.body.style.backgroundColor = 'black'
+      showAlert(" light mode", "success :")
     }
-    else{
+    else {
       setMode('light')
-      document.body.style.backgroundColor ='white'
-      showAlert(" dark mode","success :")
+      document.body.style.backgroundColor = 'white'
+      showAlert(" dark mode", "success :")
     }
   }
   return (
     <>
-  <Navbar title="TextUtiles" mode = {mode} toggleMode={toggleMode} />
-  <Alert alert={alert}/>
-  {/* <About/> */}
-  <TextForm showAlert={showAlert} heading = "Enter the Text here to analyze." mode = {mode}/>
+      <Router>
+        <Navbar title="TextUtiles" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <Routes >
+          
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text" mode={mode} />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+
+      </Router>
+
     </>
   );
 }
--- project-app/src/Components/Navbar.js
+++ project-app/src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -7,9 +8,9 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/">
           {props.title}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -24,17 +25,17 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <Link className="nav-link" to="/about">
                 {props.aboutText}
-              </a>
+              </Link>
             </li>
           </ul>
-          <form className="d-flex" role="search">
+          {/* <form className="d-flex" role="search">
             <input
               className="form-control me-2"
               type="search"
@@ -44,8 +45,8 @@ export default function Navbar(props) {
             <button className="btn btn-outline-success mx-3" type="submit">
               Search
             </button>
-          </form>
-          <div className="dropdown" data-bs-theme="light">
+          </form> */}
+          {/* <div className="dropdown" data-bs-theme="light">
             <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonLight" data-bs-toggle="dropdown" aria-expanded="false">
               Default dropdown
             </button>
@@ -71,7 +72,7 @@ export default function Navbar(props) {
               <li><hr className="dropdown-divider" /></li>
               <li><a className="dropdown-item" href="#">Separated link</a></li>
             </ul>
-          </div>
+          </div> */}
           <div
             className={`form-check form-switch text-${props.mode === "light" ? "dark" : "light"
               }`}
@@ -103,5 +104,5 @@ Navbar.prototype = {
 
 Navbar.defaultProps = {
   title: "ok",
-  aboutText: "okok",
+  aboutText: "About",
 };
